586d943f757f194e81ca34f0551c8e6ba3ad7597
yasharth45 <62794089+yasharth1@users.noreply.github.com>
2021-12-30 20:51:32 +0530

    Support file

--- cv.py
+++ cv.py
@@ -5,15 +5,15 @@ from datetime import date
 img = cv.imread('cheque.jpg')   #Load the image file into memory
 year = date.today().year - 2000
 word = num2words(1250000, lang='en_IN') + " only"
-cv.putText(img, str(date.today().day) + "/" + str(date.today().month), (274, 15), cv.FONT_HERSHEY_SIMPLEX, 0.2, (0, 0, 0), 1, cv.LINE_AA)
-cv.putText(img, str(year), (315, 15), cv.FONT_HERSHEY_SIMPLEX, 0.2, (0, 0, 0), 1, cv.LINE_AA)
-cv.putText(img, word, (54, 50), cv.FONT_HERSHEY_SIMPLEX, 0.3, (70, 70, 70), 0, cv.LINE_AA)
-cv.putText(img, "Navneet Kumar", (100, 25), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv.LINE_AA)
-cv.putText(img, "1,00,00,000", (260, 57), cv.FONT_HERSHEY_SIMPLEX, 0.2, (0, 0, 0), 1, cv.LINE_AA)
+cv.putText(img, str(date.today().day) + "/" + str(date.today().month), (776, 67), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv.LINE_AA)
+cv.putText(img, "/" + str(year), (828, 67), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv.LINE_AA)
+cv.putText(img, word, (161, 165), cv.FONT_HERSHEY_SIMPLEX, 1.2, (70, 70, 70), 0, cv.LINE_AA)
+cv.putText(img, "Navneet Kumar", (88, 125), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1, cv.LINE_AA)
+cv.putText(img, "1,00,00,000", (776, 211), cv.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 0), 1, cv.LINE_AA)
 cv.imshow("Your cheque", img)
 cv.waitKey(0)
 cv.destroyAllWindows()
-"""img = plt.imread('cheque.jpg')
+img = plt.imread('cheque.jpg')
 plt.imshow(img)
 plt.axis('on')
-plt.show()"""
+plt.show()
